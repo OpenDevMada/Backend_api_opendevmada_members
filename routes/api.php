b336ef77a327797bb430c6e52c26c225df172041
Manitriniaina2002 <manitriniaina2002@gmail.com>
2025-09-26 23:59:48 +0300

    Add CORS headers to api.php to allow cross-origin requests.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Credentials: true");
+    
+    // Handle preflight requests
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
 
     header('Content-Type: application/json; charset=utf-8');
     
